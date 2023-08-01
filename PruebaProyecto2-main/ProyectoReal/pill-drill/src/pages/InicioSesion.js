702dbb582e41ec79059d7db650309356dd97e51f
SashaCattaneo1412 <sashaacattaneo@gmail.com>
2023-08-01 08:49:18 -0300

    Initial commit

---
+++ PruebaProyecto2-main/ProyectoReal/pill-drill/src/pages/InicioSesion.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/Layout'
+ import Link from 'next/link';
+ import PhotoCarousel from '../components/PhotoCarousel.js';
+
+ const InicioSesion = () => {
+   return (
+    <div>
+ 
+     <PhotoCarousel />
+    </div>
+   )
+ }
+ 
+ export default InicioSesion
